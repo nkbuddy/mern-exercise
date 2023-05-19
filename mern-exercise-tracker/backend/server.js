6a21b75b583369a5ea36d14477ee08b38efd67a2
nkbuddy <49331641+nkbuddy@users.noreply.github.com>
2023-05-19 16:56:58 -0400

    need to fix bug

---
+++ mern-exercise-tracker/backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5050;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log("MongoDB database connection established successfully");
+});
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises',exercisesRouter);
+app.use('/users',usersRouter);
+
+app.listen(port,()=>{
+    console.log(`Server is running on port:${port}`);
+});
--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router,Route, Routes} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.componenet";
+import ExercisesList from "./components/exercises-list.componenet";
+import EditExercises from "./components/edit-exercises.componenet";
+import CreateExercise from "./components/create-exercise.componenet";
+import CreateUser from "./components/create-user.componenet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className='container'>
+    <Navbar/>
+    <br/>
+    <Routes>
+    <Route path="/" exact Component={ExercisesList}/>
+    <Route path="/edit/:id" Component={EditExercises}/>
+    <Route path="/create" Component={CreateExercise}/>
+    <Route path="/user" Component={CreateUser}/>
+    </Routes>
     </div>
+    </Router>
   );
 }
 
